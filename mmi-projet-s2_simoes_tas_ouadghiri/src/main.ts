e9803e269b496cdf93726e89797f534dab0d1ab3
angelo-simoes <angelo.simoes@edu.univ-fcomte.fr>
2023-05-16 10:22:19 +0200

    Installation GIT

---
+++ mmi-projet-s2_simoes_tas_ouadghiri/src/main.ts
@@ -0,0 +1,16 @@
+import './assets/css/style.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+import routes from '~pages'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const app = createApp(App)
+
+app.use(
+    createRouter({
+      history: createWebHistory(),
+      routes
+    })
+  )
+
+app.mount('#app')
---
+++ mmi-projet-s2_simoes_tas_ouadghiri/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      indigo: '#1d096b',
+      pink: '#feaa9b',
+      white: '#ffffff',
+      black: '#000000',
+      transparent: 'transparent',
+      gray: colors.zinc
+    },
+    fontFamily: {
+      sans: ['Mulish', 'sans-serif'],
+      serif: ['Space Mono', 'monospace']
+    },
+    extend: {
+      gridTemplateColumns: {
+        principal: 'repeat(12, minmax(0, 64px))'
+      },
+    },
+  },
+  plugins: [],
+}
+
