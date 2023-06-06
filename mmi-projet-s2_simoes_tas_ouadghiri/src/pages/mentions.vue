bbfa7ddb0cfc1dfe13bb546d7c8cab5b4bbb6b61
angelo-simoes <angelo.simoes@edu.univ-fcomte.fr>
2023-06-06 22:59:55 +0200

    rgpd + mentions légales

--- mmi-projet-s2_simoes_tas_ouadghiri/src/pages/mentions.vue
+++ mmi-projet-s2_simoes_tas_ouadghiri/src/pages/mentions.vue
@@ -1,3 +1,35 @@
 <template>
-    
+     <div class="mx-auto my-20 max-w-2xl space-y-3 px-6">
+        <div class="max-w-3xl mx-auto py-8">
+            <h1 class="text-3xl font-bold mb-4">Mentions Légales</h1>
+            
+            <h4 class="text-lg font-semibold">Éditeur de l’application :</h4>
+            <p class="mb-4">Angelo Simoes</p>
+            
+            <h4 class="text-lg font-semibold">Siège social :</h4>
+            <p class="mb-4">4 Place Tharradin, 25200 Montbéliard</p>
+            
+            <h4 class="text-lg font-semibold">Numéro de téléphone :</h4>
+            <p class="mb-4">06 51 07 19 26</p>
+            
+            <h4 class="text-lg font-semibold">Adresse e-mail :</h4>
+            <p class="mb-4">angelo.simoes@edu.univ-fcomte.fr</p>
+            
+            <h4 class="text-2xl font-bold mt-8 mb-4">Responsabilité</h4>
+            <p class="mb-4">L’éditeur ne peut être tenu responsable de tout dommage, direct ou indirect, qui pourrait résulter de l’utilisation de l’application, en particulier en ce qui concerne l’exactitude et la fiabilité des informations qui y sont disponibles. L’utilisateur est seul responsable de l’utilisation qu’il fait de l’application.</p>
+            
+            <h4 class="text-2xl font-bold mt-8 mb-4">Propriété intellectuelle</h4>
+            <p class="mb-4">Tous les éléments constituant l’application (notamment les textes, graphismes, logos, images, vidéos, sons, icônes) sont la propriété exclusive de l’éditeur ou de ses partenaires et sont protégés par les lois françaises et internationales relatives à la propriété intellectuelle. Toute reproduction, représentation, modification, publication, transmission, dénaturation, totale ou partielle de l’application ou de son contenu, par quelque procédé que ce soit, et sur quelque support que ce soit, est interdite.</p>
+            
+            <h4 class="text-2xl font-bold mt-8 mb-4">Données personnelles</h4>
+            <p class="mb-4">L’application peut être amenée à collecter des données personnelles de ses utilisateurs. L’éditeur s’engage à respecter la réglementation en vigueur en matière de protection des données personnelles. Les utilisateurs disposent d’un droit d’accès, de rectification et de suppression des données les concernant, ainsi que d’un droit d’opposition à leur traitement. Ces droits peuvent être exercés en contactant l’éditeur à l’adresse e-mail mentionnée ci-dessus.</p>
+            
+            <h4 class="text-2xl font-bold mt-8 mb-4">Hébergement</h4>
+            <p class="mb-4">L’application est hébergée par Angelo Simoes, dont le siège social est situé à 4 Place Tharradin, 25200 Montbéliard.</p>
+            
+            <h4 class="text-2xl font-bold mt-8 mb-4">Loi applicable</h4>
+            <p class="mb-4">Les présentes mentions légales sont soumises au droit français. Tout litige relatif à l’interprétation ou à l’exécution des présentes mentions légales sera soumis à la compétence exclusive des tribunaux français.
+            Nous vous recommandons de faire appel à un avocat pour vous assurer que ces mentions légales sont conformes à la législation en vigueur et à votre situation particulière.</p>
+        </div>
+     </div>
 </template>
--- mmi-projet-s2_simoes_tas_ouadghiri/src/pages/rgpd.vue
+++ mmi-projet-s2_simoes_tas_ouadghiri/src/pages/rgpd.vue
@@ -1,3 +1,66 @@
 <template>
-    
+    <div class="mx-auto my-20 max-w-2xl space-y-3 px-6">
+        <div class="max-w-3xl mx-auto py-8">
+            <h1 class="text-2xl font-bold mb-4">Règlement général sur la protection des données</h1>
+            
+            <h4 class="text-lg font-semibold">Introduction</h4>
+            <p class="mb-4">Le Règlement général sur la protection des données (RGPD) est une réglementation de l’Union européenne qui vise à protéger les données personnelles des individus. En tant qu’éditeur de l’application "CooHop!", nous nous engageons à respecter les dispositions du RGPD pour assurer la sécurité et la confidentialité des données de nos utilisateurs.</p>
+            
+            <h4 class="text-lg font-semibold">1. Responsable du traitement</h4>
+            <p class="mb-4">L’éditeur de l’application "CooHop!" est le responsable du traitement des données personnelles des utilisateurs de l’application. Nous avons désigné un délégué à la protection des données (DPO) qui peut être contacté à l’adresse e-mail suivante : angelo.simoes@edu.univ-fcomte.fr.</p>
+            
+            <h4 class="text-lg font-semibold">2. Les données personnelles collectées</h4>
+            <p class="mb-4">Les données personnelles collectées par l’application "CooHop!" sont les suivantes :</p>
+            <ul class="list-disc pl-6 mb-4">
+                <li>Les informations d’inscription : nom, prénom, adresse e-mail, mot de passe, photo de profil, âge, sexe, poids, taille, niveau sportif, localisation.</li>
+                <li>Les messages échangés via la messagerie intégrée à l’application.</li>
+                <li>Les données relatives à l’utilisation de l’application : historique des activités sportives, recherche de partenaires sportifs, notifications.</li>
+            </ul>
+            
+            <h4 class="text-lg font-semibold">3. Finalités de la collecte et du traitement des données</h4>
+            <p class="mb-4">Les données personnelles collectées sont utilisées pour les finalités suivantes :</p>
+            <ul class="list-disc pl-6 mb-4">
+                <li>Permettre la mise en relation de personnes cherchant des partenaires sportifs en fonction de leur localisation et de leur niveau sportif.</li>
+                <li>Permettre aux utilisateurs de communiquer entre eux via la messagerie intégrée.</li>
+                <li>Améliorer l’expérience utilisateur en proposant des suggestions d’activités sportives en fonction de l’historique des activités sportives de l’utilisateur.</li>
+                <li>Envoyer des notifications liées aux activités sportives et à la messagerie.</li>
+            </ul>
+            
+            <h4 class="text-lg font-semibold">4. Destinataires des données personnelles</h4>
+            <p class="mb-4">Les données personnelles collectées par l’application "CooHop!" ne sont pas transmises à des tiers, sauf dans les cas suivants :</p>
+            <ul class="list-disc pl-6 mb-4">
+                <li>Lorsque cela est nécessaire pour répondre à une demande de l’utilisateur (par exemple, pour trouver un partenaire sportif).</li>
+                <li>Lorsque cela est requis par la loi ou une décision judiciaire.</li>
+            </ul>
+            
+            <h4 class="text-lg font-semibold">5. Durée de conservation des données</h4>
+            <p class="mb-4">Les données personnelles des utilisateurs sont conservées pendant la durée nécessaire à la réalisation des finalités pour lesquelles elles ont été collectées. Les données sont ensuite supprimées ou anonymisées.</p>
+            
+            <h4 class="text-lg font-semibold">6. Droits des utilisateurs</h4>
+            <p class="mb-4">Les utilisateurs disposent des droits suivants concernant leurs données personnelles :</p>
+            <ul class="list-disc pl-6 mb-4">
+                <li>Droit d’accès : les utilisateurs ont le droit d’obtenir une copie des données personnelles les concernant.</li>
+                <li>Droit de rectification : les utilisateurs ont le droit de demander la rectification des données personnelles les concernant.</li>
+                <li>Droit à l’effacement : les utilisateurs ont le droit de demander l’effacement des données personnelles les concernant.</li>
+                <li>Droit à la limitation du traitement : les utilisateurs ont le droit de demander la limitation du traitement de leurs données personnelles.</li>
+                <li>Droit à la portabilité des données : les utilisateurs ont le droit de recevoir les données personnelles les concernant dans un format structuré, couramment utilisé et lisible par machine, et de les transmettre à un autre responsable de traitement sans obstacle de notre part.</li>
+                <li>Droit d’opposition : les utilisateurs ont le droit de s’opposer à tout moment, pour des raisons tenant à leur situation particulière, au traitement de leurs données personnelles.</li>
+                <li>Droit de retirer le consentement : lorsque le traitement est fondé sur le consentement de l’utilisateur, ce dernier a le droit de retirer son consentement à tout moment.</li>
+            </ul>
+            
+            <p class="mb-4">Les utilisateurs peuvent exercer leurs droits en nous contactant à l’adresse e-mail suivante : angelo.simoes@edu.univ-fcomte.fr. Nous nous engageons à répondre à toute demande dans un délai raisonnable et à fournir toute information ou assistance nécessaire pour faciliter l’exercice de ces droits.</p>
+            
+            <h4 class="text-lg font-semibold">7. Sécurité des données</h4>
+            <p class="mb-4">Nous prenons toutes les mesures techniques et organisationnelles appropriées pour assurer la sécurité et la confidentialité des données personnelles des utilisateurs, notamment en limitant l’accès aux données aux seules personnes autorisées et en protégeant les données contre les accès non autorisés, les pertes, les destructions ou les altérations.</p>
+            
+            <h4 class="text-lg font-semibold">8. Modifications du règlement</h4>
+            <p class="mb-4">Nous nous réservons le droit de modifier le présent règlement RGPD à tout moment. Les modifications seront notifiées aux utilisateurs par voie électronique ou via l’application. Tout utilisateur continuant à utiliser l’application après l’entrée en vigueur des modifications sera réputé avoir accepté les nouvelles dispositions du règlement.</p>
+            
+            <h4 class="text-lg font-semibold">9. Contact</h4>
+            <p>Pour toute question ou réclamation relative à la protection des données personnelles, les utilisateurs peuvent nous contacter à l’adresse e-mail suivante : angelo.simoes@edu.univ-fcomte.fr</p>
+            
+            <p class="mt-8">Fait à Montbéliard, le 20/04/2023<br>L’équipe de l’application "CooHop!"</p>
+        </div>
+
+    </div>
 </template>
